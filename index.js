8efe453adc4450340d32ba678f9f41f5232567ab
otoneko1102 <rin_pineapple@au.com>
2024-12-18 12:52:50 +0900

    Add JSDOC.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * @function 
+ * Returns a buffer randomly.
+ * @param {number} [length=0] - The length of the buffer. 
+ * @returns {Buffer} Returns a random buffer.
+ */
 function buffer(length = 0) {
   if (!Number.isInteger(length) || length < 0) throw new Error('length must be non-negative number.');
   const result = Buffer.alloc(length);
